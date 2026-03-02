71a7426018d961c830401b5600b154367b2ae80c
Shivamingale3 <shivamingale3@gmail.com>
2026-03-03 00:03:32 +0530

    feat!: Remove UserNote model and all related database and UI components.

--- lib/database/isar.dart
+++ lib/database/isar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:isar_community/isar.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:notes_vault/database/models/user_note.dart';
 
 class IsarDb {
   static late Isar isar;
@@ -9,10 +8,6 @@ class IsarDb {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
     final dir = await getApplicationDocumentsDirectory();
-    isar = await Isar.open(
-      [UserNoteSchema],
-      directory: dir.path,
-      inspector: true,
-    );
+    isar = await Isar.open([], directory: dir.path, inspector: true);
   }
 }
